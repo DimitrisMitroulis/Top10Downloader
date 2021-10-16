b79a1d04641ca2952975564252cb0f265d1c2e01
DimitrisMitroulis <amfimitrul@gmail.com>
2021-10-16 23:20:41 +0300

    bug fixes

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.top10downloader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideNavBar();
     }
+    private void hideNavBar() {
+        getSupportActionBar().hide();
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                );
+    }
+
 }
