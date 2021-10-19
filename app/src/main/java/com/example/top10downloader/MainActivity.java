1744bb80c9f680f5667fcd4175c52bfd0e68b168
DimitrisMitroulis <amfimitrul@gmail.com>
2021-10-19 11:25:40 +0300

    added method

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideNavBar();
+        mainVoid();
     }
     private void hideNavBar() {
         getSupportActionBar().hide();
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 );
+    }
+    private void mainVoid(){
+
+
     }
 
 }
