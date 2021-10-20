7e26a88995098acbf516ac87093647c749e21777
DimitrisMitroulis <amfimitrul@gmail.com>
2021-10-20 23:22:00 +0300

    save state

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "main activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideNavBar();
+        Log.d(TAG, "onCreate: starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL");
+        Log.d(TAG, "onCreate: Done");
 
 
     }
-
 
     private void hideNavBar() {
         Objects.requireNonNull(getSupportActionBar()).hide();
@@ -31,14 +37,19 @@ public class MainActivity extends AppCompatActivity {
     private class DownloadData extends AsyncTask<String, Void, String>
 
     {//String is url to rss feed , where void is for displaying progress bars,  String for return
+        private static final String TAG = "Download";
+
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is "+s);
         }
+
 
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            Log.d(TAG, "doInBackground:  starts with "+ strings[0]);
+            return "do in background Completed";
         }
     }
 
