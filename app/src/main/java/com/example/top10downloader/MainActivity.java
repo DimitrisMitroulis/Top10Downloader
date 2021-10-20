efe6894d14b9dbd70cb7540d3e6b47913aa8e0a2
DimitrisMitroulis <amfimitrul@gmail.com>
2021-10-20 22:39:52 +0300

    implemented methods

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.top10downloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,21 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         hideNavBar();
-        mainVoid();
+
 
     }
+
+
     private void hideNavBar() {
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         this.getWindow().getDecorView()
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 );
     }
-    private void mainVoid(){
 
+    private class DownloadData extends AsyncTask<String, Void, String>
 
+    {//String is url to rss feed , where void is for displaying progress bars,  String for return
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
+
 
 }
