6f0522987620c292603fe83f1c7f3690b32cec44
DimitrisMitroulis <amfimitrul@gmail.com>
2021-10-20 21:29:44 +0300

    changed icon, theme changes

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         hideNavBar();
         mainVoid();
+
     }
     private void hideNavBar() {
         getSupportActionBar().hide();
